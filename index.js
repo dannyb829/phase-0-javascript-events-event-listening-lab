8af56d8689fc90c8c512280940bacfe327cbbdf8
Daniel <dannyb829@gmail.com>
2022-03-01 16:26:34 -0500

    commit tree 41a6be5d59a5c412c8a60054f5dc9ec52cc20093

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const clickAlert = () => {alert('I was clicked')}
+
+
 function addingEventListener() {
+    const input = document.getElementById(`input`);
+    input.addEventListener('click', () => clickAlert)
 }
+
+addingEventListener()
